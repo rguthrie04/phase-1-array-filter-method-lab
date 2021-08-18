b846ecdb8d487929921fec6a8ea2fd4ec595cbf6
Ryan Guthrie <ryanguthrie@zoho.com>
2021-08-18 23:13:07 +0100

    commit tree 944ef9134d591ed3ae6bded1ebf6a27d3bb1bbea

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+//findMatching()
+//function that returns all drivers that match the passed in name
+//returns matching drivers if case does not match but letters do - toLowerCase?
+//returns an empty array if there is no match
+
+function findMatching(drivers, driverName){
+    const result = drivers.filter(newName => newName.toLowerCase() === driverName.toLowerCase())
+    return result
+}
+
+function fuzzyMatch(drivers, firstTwoLet){
+    const result = drivers.filter(fuzzyName => fuzzyName.substring(0,2) === firstTwoLet.substring(0,2))
+    return result
+}
+
+function matchName(arrayOfDriverObjects,stringName) {
+    const result = arrayOfDriverObjects.filter(names => names.name === stringName)
+    return result
+}
